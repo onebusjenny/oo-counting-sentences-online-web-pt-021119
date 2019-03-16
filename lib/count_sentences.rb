6b8a093c7cc3bfd10681d57c1ddc5b93ddd5c1d9
Jenny Chu <jchudesign0930@gmail.com>
2019-03-16 03:53:37 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+   
+    "HI".end_with?("!")
+  binding.pry
+   
   end
 
   def question?
-
+"Hi".end_with?("?")
   end
 
   def exclamation?
-
+"Hi".end_with?("?")
   end
 
   def count_sentences
-
+"Hi".end_with!("!")
   end
 end
