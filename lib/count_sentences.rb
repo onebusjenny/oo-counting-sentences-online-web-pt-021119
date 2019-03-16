8b6f028f9042376900ca24cc10ebf1f96825c3d4
Jenny Chu <jchudesign0930@gmail.com>
2019-03-16 04:23:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,14 @@ self.end_with?("?")
 self.end_with?("!")
   end
 
-  def count_sentences
-self.split(/[.?!]/).count
+def count_sentences
+    count = 0
+    self.each_char.with_index do |char, i|
+      if (char == "." || char == "!" || char == "?") && (self[i + 1] != "." && self[i + 1] != "!" && self[i + 1] != "?")
+        count += 1
+      end
+    end
+    count
   end
+ 
 end
